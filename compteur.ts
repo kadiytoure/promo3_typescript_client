fc7644165d4eafd7c243bed61ffcd621673738d3
toure <tourekadiy@gmail.com>
2017-09-12 10:20:11 +0200

    new files;

---
+++ compteur.ts
@@ -0,0 +1,13 @@
+
+
+export class Compteur {
+    constructor(private compte:number=0){}
+
+    increment() {
+        this.compte++;
+    }
+   
+    showCount():number {
+        return this.compte;
+    }
+}
