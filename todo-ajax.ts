129114985778b63871f7e633108d2e2f218fe5f6
toure <tourekadiy@gmail.com>
2017-09-13 11:27:58 +0200

    class TodoAjax with API;

--- todo-ajax.ts
+++ todo-ajax.ts
@@ -1,1 +1,11 @@
 import * as $ from 'jquery';
+
+class TodoAjax {
+   private urlApi:string = 'http://localhost:3000/todo';
+
+   getAllTodos():Promise<string[]> {
+       return $.ajax({
+           url: this.urlApi
+       });
+   }
+}
